09f23adcca73d839847037872f4398915c74c22b
PavankumarAN <pavankumaran51@gmail.com>
2019-05-10 23:46:45 +0530

    Images

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +12,38 @@ app.get('/', (req,res) => {
     });
 });
 
-app.use(express.static(__dirname+ '/public'));
+app.get('/:area', (req,res) => {
+    const areaName = req.params.area;
+    var title, heading;
+    var imageCount = 4;
+    if(areaName=== "koramangala") {
+        title : "Koramangala"; 
+        heading : "Famous for pubs";
+    } else if(areaName === "jpnagar") {
+        title : "JP Nagar";
+        heading : "Celebrities area";   
+    }else if(areaName === "btm") {
+        title : "BTM";
+        heading : "Famous for PGs";
+    } else if(areaName === "jayanagar") {
+        title : "Jayanagara";
+        heading : "Most famous cool area";
+     }
+   // else if(areaName === "rajajinagar") {
+    //     title : "Rajajinagar";
+    //     heading : "Famous for citizen friendly MLA";
+    //     //imageCount : 6;
+    // }
+
+    res.render('area', {
+        title : title,
+        headline : heading,
+        area : areaName,
+        numberOfImages : imageCount
+    });
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT,() => {
     console.log(`app is running at port : ${PORT}`);
