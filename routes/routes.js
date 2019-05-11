9fc2fb3969e3819e1c1be56bddc96b088415ab9f
PavankumarAN <pavankumaran51@gmail.com>
2019-05-11 21:18:05 +0530

    Correcting the title and heading part in the routes file

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-module.exports.home = (req,res) => {
+exports.home = (req,res) => {
     res.render('home', {
         title : "Bengaluru",
         headline : 'We are always on a Clean Bengaluru mission'
@@ -6,22 +6,22 @@ module.exports.home = (req,res) => {
 }
 
 
-module.exports.area = (req,res) => {
-    const areaName = req.params.area;
+exports.area = (req,res) => {
+    var areaName = req.params.area;
     var title, heading;
     var imageCount = 4;
     if(areaName=== "koramangala") {
-        title : "Koramangala"; 
-        heading : "Famous for pubs";
+        title = "Koramangala";
+        heading = "Famous for pubs";
     } else if(areaName === "jpnagar") {
-        title : "JP Nagar";
-        heading : "Celebrities area";   
+        title = "JP Nagar";
+        heading = "Celebrities area";   
     }else if(areaName === "btm") {
-        title : "BTM";
-        heading : "Famous for PGs";
+        title = "BTM";
+        heading = "Famous for PGs";
     } else if(areaName === "jayanagar") {
-        title : "Jayanagara";
-        heading : "Most famous cool area";
+        title = "Jayanagara";
+        heading = "Most famous cool area";
      }
    
 
